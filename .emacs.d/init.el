658cf51ede4e29200aa2c348daeced1e6744835f
Jeremy Durham <jdurham@jeremy-durhams-imac.local>
2010-02-23 17:44:19 -0500

    Added comments and config for ORG mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/")
 (set-frame-parameter nil 'alpha '(85 75))
 
+; Twitter
 (require 'twitter)
 (autoload 'twitter-get-friends-timeline "twitter" nil t)
 (autoload 'twitter-status-edit "twitter" nil t)
@@ -23,27 +24,33 @@ point."
 (defun my-tab-fix ()
   (local-set-key [tab] 'indent-or-expand))
 
+; Smart tab completion
 (add-hook 'ruby-mode-hook       'my-tab-fix)
 
+; Add the twilight theme (ala Textmate)
 (require 'color-theme)
 (color-theme-initialize)
 (color-theme-twilight)
 
+; Custom set term colors (black background, white text)
 (custom-set-variables
- '(term-bind-key-alist (quote (("C-y" . term-paste))))
  '(term-default-bg-color "#000000")
  '(term-default-fg-color "#FFFFFF"))
 
+; Set tab width and standard indentation to 2 spaces
 (setq-default tab-width 2
               standard-indent 2
               indent-tabs-mode nil)
 
+; Global clipboard changes
 (global-set-key "\C-w" 'clipboard-kill-region)
 (global-set-key "\M-w" 'clipboard-kill-ring-save)
 (global-set-key "\C-y" 'clipboard-yank)
 
+; ANSI in terminals (shell)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
+; Allows you to press 'y' or 'n' vs 'yes' or 'no'
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ; Disable backups on files
@@ -58,10 +65,6 @@ point."
 (global-hl-line-mode 1)
 (set-face-background 'hl-line "#222")
 
-(defun gf (file_name)
-  (interactive "sFile? ")
-  (find-file (concat "~/.emacs.d/" file_name)))
-
 (require 'magit)
 
 (ido-mode t)
@@ -74,8 +77,21 @@ point."
                 (ido-completing-read "Project file: "
                                      (tags-table-files) nil t)))))
 
+; Shortcut for find-file-in-project
 (global-set-key (kbd "C-c t") 'Ido-find-file-in-tag-files)
+
+; Shortcut for magit
 (global-set-key (kbd "C-c m") 'magit-status)
 
+; IBuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (autoload 'ibuffer "ibuffer" "List buffers." t)
+
+; Transient mark mode (marked area highlighting)
+(transient-mark-mode t)
+
+; ORG mode
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
